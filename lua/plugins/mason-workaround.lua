eca2ca19492a5529be4ee58e7e49ec0d4bc1d782
Matheus Morillo <matheus.morillo@nubank.com.br>
2025-09-02 16:07:36 -0300

    ensure common lsps are installed + configure tailwind lsp to match tv()

--- lua/plugins/mason-workaround.lua
+++ lua/plugins/mason-workaround.lua
@@ -1,4 +1,43 @@
 return {
-  { "mason-org/mason.nvim", version = "1.11.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
+  {
+    "mason-org/mason.nvim", 
+    version = "1.11.0",
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    version = "1.32.0",
+    config = function()
+      require("mason-lspconfig").setup({
+        -- Automatically install these LSP servers (based on currently installed)
+        ensure_installed = {
+          "lua_ls",                    -- Lua (lua-language-server)
+          "tailwindcss",               -- TailwindCSS (tailwindcss-language-server)
+          "tsserver",                  -- TypeScript/JavaScript (typescript-language-server)
+          "eslint",                    -- ESLint (eslint-lsp/vscode-eslint-language-server)
+          "jsonls",                    -- JSON (json-lsp/vscode-json-language-server)
+          "gopls",                     -- Go
+          "clojure_lsp",               -- Clojure (clojure-lsp)
+          "marksman",                  -- Markdown (marksman)
+          -- Additional useful servers you might want:
+          "html",                      -- HTML
+          "cssls",                     -- CSS
+          "rust_analyzer",             -- Rust
+          "pyright",                   -- Python
+        },
+        -- Automatically install when entering a buffer with the filetype
+        automatic_installation = true,
+      })
+    end,
+  },
 }
