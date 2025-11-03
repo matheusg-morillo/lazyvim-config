ea563c969d6bcd41da089b7f94e9f9cc26c71e3d
Matheus Morillo <matheus.morillo@nubank.com.br>
2025-11-03 17:15:40 -0300

    update lazyvim

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        -- Automatically install these LSP servers (based on currently installed)
+        ensure_installed = {
+          "lua_ls", -- Lua (lua-language-server)
+          "tailwindcss", -- TailwindCSS (tailwindcss-language-server)
+          "tsserver", -- TypeScript/JavaScript (typescript-language-server)
+          "eslint", -- ESLint (eslint-lsp/vscode-eslint-language-server)
+          "jsonls", -- JSON (json-lsp/vscode-json-language-server)
+          "gopls", -- Go
+          "clojure_lsp", -- Clojure (clojure-lsp)
+          "marksman", -- Markdown (marksman)
+          -- Additional useful servers you might want:
+          "html", -- HTML
+          "cssls", -- CSS
+          "rust_analyzer", -- Rust
+          "pyright", -- Python
+        },
+        -- Automatically install when entering a buffer with the filetype
+        automatic_installation = true,
+      })
+    end,
+  },
+}
