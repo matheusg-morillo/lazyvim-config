2ae44815a6c29a0fdc670da644baa6523355be59
Matheus Morillo <matheus.morillo@nubank.com.br>
2025-11-11 10:25:25 -0300

    fix treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,46 +1,39 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPost", "BufNewFile" }, -- lazy-load to avoid bootstrap races
   build = ":TSUpdate",
+  opts = {
+    highlight = { enable = true },
+    indent = { enable = true },
+    fold = { enable = true },
+    ensure_installed = {
+      "lua",
+      "javascript",
+      "python",
+      "dart",
+      "ts_ls",
+      "go",
+      "yaml",
+      "css",
+      "html",
+      "regex",
+      "java",
+      "rust",
+      "json",
+      "jsonc",
+      "bash",
+      "toml",
+      "tsx",
+      "vue",
+      "svelte",
+      "graphql",
+      "scss",
+      "php",
+      "ruby",
+      "markdown",
+    },
+  },
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "clojure",
-        "lua",
-        "javascript",
-        "python",
-        "dart",
-        "typescript",
-        "go",
-        "yaml",
-        "css",
-        "html",
-        "regex",
-        "java",
-        "rust",
-        "json",
-        "jsonc",
-        "bash",
-        "toml",
-        "tsx",
-        "vue",
-        "svelte",
-        "graphql",
-        "scss",
-        "php",
-        "ruby",
-        "markdown",
-      },
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-      fold = {
-        enable = true,
-      },
-    })
-
     vim.opt.foldmethod = "expr"
     vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
     vim.opt.foldenable = true
