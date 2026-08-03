1c3a0281f7acdad0d381918c368c4d2f8b6521ba
Matheus Morillo <matheus.morillo@nubank.com.br>
2026-08-03 14:39:08 -0300

    add claudecode dep

---
+++ lua/plugins/claudecode.lua
@@ -0,0 +1,3 @@
+return {
+  "coder/claudecode.nvim",
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,37 @@
 return {
-  "princejoogie/dir-telescope.nvim",
-  config = function()
-    require("dir-telescope").setup({
-      -- these are the default options set
-      hidden = true,
-      no_ignore = false,
-      show_preview = true,
-      follow_symlinks = false,
-    })
-  end,
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+    },
+    config = function()
+      local telescope = require("telescope")
+
+      telescope.setup({
+        defaults = {
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--fixed-strings",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "princejoogie/dir-telescope.nvim",
+    config = function()
+      require("dir-telescope").setup({
+        hidden = true,
+        no_ignore = false,
+        show_preview = true,
+        follow_symlinks = false,
+      })
+    end,
+  },
 }
