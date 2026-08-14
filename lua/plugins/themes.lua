22f3586a17584ee1c7ec1a855c22a832b283fa2b
Matheus Morillo <matheus.morillo@nubank.com.br>
2026-08-14 19:08:46 -0300

    disable mouse support

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,7 +1,7 @@
 return {
-  -- add nightfox
   { "EdenEast/nightfox.nvim" },
   { "folke/tokyonight.nvim" },
+  { "scottmckendry/cyberdream.nvim" },
   { "navarasu/onedark.nvim" },
   {
     "miversen33/sunglasses.nvim",
